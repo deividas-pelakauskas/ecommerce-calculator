598e6eb5385cd02cf8ebbd55b4d49417049a69fa
Deividas Pelakauskas <pelakauskas1@gmail.com>
2020-02-22 01:21:42 +0000

    __init__ method

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # Test by Deividas Pelakauskas
+
+class Product:
+
+    def __init__(self, sku, product_name, supplier, quantity):
+        self.sku = sku
+        self.product_name = product_name
+        self.supplier = supplier
+        self.quantity = quantity
